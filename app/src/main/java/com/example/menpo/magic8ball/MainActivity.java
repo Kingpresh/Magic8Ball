e039af685c9c80e1605c600ab97ffd410ba71c58
KingPresh <emeka.michael0000@gmail.com>
2020-04-08 22:50:49 +0100

    Magic 8 Ball App

---
+++ app/src/main/java/com/example/menpo/magic8ball/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.menpo.magic8ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton = findViewById(R.id.button);
+
+        final ImageView image = findViewById(R.id.imageView);
+
+        final int[] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("8 Ball","The app works!");
+
+                Random randomNumber = new Random();
+                int number = randomNumber.nextInt(5);
+
+                image.setImageResource(ballArray[number]);
+
+                Log.d("8 ball","Random number is " + number);
+
+            }
+        });
+
+    }
+}
